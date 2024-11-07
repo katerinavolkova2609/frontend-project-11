98ac7affe4e6ef5029e574e9b163ad687b6df317
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2024-11-07 17:43:45 +0300

    add parsing rss

--- src/application.js
+++ src/application.js
@@ -1,8 +1,11 @@
 import * as yup from 'yup';
-import onChange from 'on-change';
+// import onChange from 'on-change';
 import watch from './view';
 import i18next from 'i18next';
 import resources from './locales/index';
+import axios from 'axios';
+import parse from './parser';
+import { uniqId } from 'lodash';
 
 export default () => {
   const state = {
@@ -12,26 +15,35 @@ export default () => {
   };
   const i18n = i18next.createInstance();
 
-  i18n
-    .init({
-      lng: 'ru',
-      debug: true,
-      resources,
-    })
-    .then(function (t) {
-      // initialized and ready to go!
-      //   document.getElementById('output').innerHTML = i18next.t('key');
-    });
+  i18n.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  });
+  // .then(function (t) {
+  //   // initialized and ready to go!
+  //   //   document.getElementById('output').innerHTML = i18next.t('key');
+  // });
+
   yup.setLocale({
     mixed: {
       notOneOf: i18n.t('errors.validation.uniqURL'),
     },
     string: {
       url: i18n.t('errors.validation.invalidURL'),
-
     },
-
   });
+
+  const makeProxyURL = (link) =>
+    `https://allorigins.hexlet.app/get?disableCache=true&url=${link}`;
+
+  const getData = (link) =>
+    axios.get(makeProxyURL(link)).then((response) => {
+      const parsedData = parse(response.data.contents);
+      console.log(response);
+      return response;
+    });
+
   const validateSchema = (links) => {
     const schema = yup.string().url().notOneOf(links);
     return schema;
@@ -40,24 +52,24 @@ export default () => {
   const form = document.querySelector('form');
   const watchedState = watch(state);
 
-  form.addEventListener('submit', async (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const inputValue = formData.get('url');
-    await validateSchema(state.feeds)
+    validateSchema(state.feeds)
       .validate(inputValue)
+      .then(() => {
+        if (watchedState.isValid !== false) {
+          state.feeds.push(inputValue);
+          console.log(state.feeds);
+          getData(inputValue);
+        }
+      })
       .catch((e) => {
         watchedState.isValid = false;
         console.log(e.errors);
       });
-    // console.log(answer);
-    // if (answer === false) {
-    //   watchedState.isValid = false;
-    // } else {
-    if (watchedState.isValid !== false) {
-      state.feeds.push(inputValue);
-    }
+    // if (watchedState.isValid !== false) {
     // }
-    console.log(state.feeds);
   });
 };
---
+++ src/parser.js
@@ -0,0 +1,27 @@
+export default (stringContainingRSS) => {
+  const parser = new DOMParser();
+  const content = parser.parseFromString(
+    stringContainingRSS,
+    'application/xml'
+  );
+  console.log(content);
+  if (content.querySelector('parsererror')) {
+    throw new Error('parse error');
+  }
+
+  const rootEl = content.querySelector('channel');
+  const mainTitle = rootEl.querySelector('title').textContent;
+  const mainFeedDescription = rootEl.querySelector('description').textContent;
+
+  const items = Array.from(root.querySelectorAll('item'));
+  const posts = items.map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    return { title, description };
+  });
+
+  return {
+    feeds: { title: mainTitle, description: mainFeedDescription },
+    posts: posts,
+  };
+};
